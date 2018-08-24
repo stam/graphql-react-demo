2b1d6a42b30d78ced32feca66272be58981b7bf2
Jasper Stam <jasper.stam@philips.com>
2018-08-24 15:09:55 +0200

    Cleanup subscribeToMore mess

--- src/CommentFeed.js
+++ src/CommentFeed.js
@@ -4,17 +4,6 @@ import { Query } from 'react-apollo';
 
 import CommentOverview from './CommentOverview';
 
-const COMMENTS_SUBSCRIPTION = gql`
-  subscription {
-    comments {
-      comment
-      createdOn
-      authorName
-      talkTitle
-    }
-  }
-`;
-
 const COMMENTS_QUERY = gql`
   query {
     comments(page: {
@@ -41,46 +30,7 @@ const CommentFeed = () => (
   <Query
     query={COMMENTS_QUERY}
   >
-    {({ subscribeToMore, ...result }) => (
-        <CommentOverview
-          {...result}
-          subscribeToNewComments={() =>
-            subscribeToMore({
-              document: COMMENTS_SUBSCRIPTION,
-              updateQuery: (previousResult, { subscriptionData }) => {
-                if (!subscriptionData.data) return previousResult;
-                const subscriptionResponse = subscriptionData.data.comments;
-
-                const { talkTitle, authorName, ...commentData } = subscriptionResponse;
-
-                // The comment from the subscription returns talkTitle and authorName flattened.
-                // Convert it to nested syntax to match query response
-                // Also apollo-client crashes hard when we don't specifically define the __typenames....
-                const newComment = {
-                  id: Date.now(), // Please add this to the contentUpdate response
-                  talk: {
-                    title: talkTitle,
-                    __typename: 'Talk',
-                  },
-                  author: {
-                    name: authorName,
-                    __typename: 'Person',
-                  },
-                  ...commentData,
-                }
-
-                return Object.assign({}, previousResult, {
-                  comments: {
-                    __typename: 'CommentPageableResponse',
-                    content: [...previousResult.comments.content, newComment]
-                  }
-                });
-              }
-            })
-          }
-        />
-    )
-    }
+    {props => <CommentOverview {...props} />}
   </Query>
 );
 
--- src/CommentOverview.js
+++ src/CommentOverview.js
@@ -1,4 +1,16 @@
 import React, { Component } from 'react';
+import gql from 'graphql-tag';
+
+const COMMENTS_SUBSCRIPTION = gql`
+  subscription {
+    comments {
+      comment
+      createdOn
+      authorName
+      talkTitle
+    }
+  }
+`;
 
 const Comment = (props) => {
   const { data: {
@@ -18,8 +30,53 @@ const Comment = (props) => {
 
 class CommentOverview extends Component {
   componentDidMount() {
-    this.props.subscribeToNewComments();
+    this.subscribeToNewComments();
   }
+
+  subscribeToNewComments() {
+    const { subscribeToMore } = this.props;
+
+    subscribeToMore({
+      document: COMMENTS_SUBSCRIPTION,
+      updateQuery: this.handleUpdate,
+    })
+  }
+
+  handleUpdate = (previousResult, { subscriptionData }) => {
+    if (!subscriptionData.data) {
+      return previousResult;
+    }
+
+    const newComment = this.sanitizeCommentUpdateReponse(subscriptionData);
+
+    return Object.assign({}, previousResult, {
+      comments: {
+        __typename: 'CommentPageableResponse',
+        content: [...previousResult.comments.content, newComment]
+      }
+    });
+  }
+
+  sanitizeCommentUpdateReponse(subscriptionData) {
+    const { talkTitle, authorName, ...commentData } = subscriptionData.data.comments;
+
+    // The comment from the subscription returns talkTitle and authorName flattened.
+    // Convert it to nested syntax to match query response
+    // Also apollo-client crashes hard when we don't specifically define the __typenames....
+    return {
+      id: Date.now(), // Please add this to the contentUpdate response
+      talk: {
+        title: talkTitle,
+        __typename: 'Talk',
+      },
+      author: {
+        name: authorName,
+        __typename: 'Person',
+      },
+      ...commentData,
+    }
+  }
+
   render() {
     const { data, loading } = this.props;
 
