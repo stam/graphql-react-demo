90a277e38d7ddbfd6449b8d8e1efa176402250f8
Jasper Stam <jasper.stam@philips.com>
2018-08-24 11:25:39 +0200

    Remove default CRA styling, add styled-components

---
+++ src/ConferenceOverview.js
@@ -0,0 +1,18 @@
+import React from 'react';
+// import { gql } from 'apollo-boost';
+// import { Query } from 'react-apollo';
+
+// const GET_CONFERENCES = gql`
+//   query {
+//     conferences {
+//       id
+//       name
+//     }
+//   }
+// `;
+
+const ConferenceOverview = () => (
+    <div>Conference Overview</div>
+)
+
+export default ConferenceOverview;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { ApolloProvider } from 'react-apollo';
+import ApolloClient from 'apollo-boost';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+// Pass your GraphQL endpoint to uri
+const client = new ApolloClient({ uri: 'https://localhost:8080/graphql' });
+
+const ApolloApp = AppComponent => (
+  <ApolloProvider client={client}>
+    <AppComponent />
+  </ApolloProvider>
+);
+ 
+render(ApolloApp(App), document.getElementById('root'));
