5208fa2165bdccdffa9622ea1fd6b186502448aa
Jasper Stam <jasper.stam@philips.com>
2018-08-24 13:11:12 +0200

    Add websocket link and improve ApolloClient configuration

--- src/index.js
+++ src/index.js
@@ -1,37 +1,46 @@
 import React from 'react';
 import { render } from 'react-dom';
-import './index.css';
-import App from './App';
 import { ApolloProvider } from 'react-apollo';
-
-
 import { ApolloClient } from 'apollo-client';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { HttpLink } from 'apollo-link-http';
-import { onError } from 'apollo-link-error';
-import { ApolloLink } from 'apollo-link';
+import { WebSocketLink } from 'apollo-link-ws';
+import { getMainDefinition } from 'apollo-utilities';
+import { split } from 'apollo-link';
+
+import './index.css';
+import App from './App';
+
+const httpLink = new HttpLink({
+  uri: 'http://localhost:8080/graphql',
+  credentials: 'same-origin'
+});
+
+const wsLink = new WebSocketLink({
+  uri: `ws://localhost:8080/subscription`,
+  options: {
+    reconnect: true
+  }
+});
+
+// using the ability to split links, you can send data to each link
+// depending on what kind of operation is being sent
+const link = split(
+  // split based on operation type
+  ({ query }) => {
+    const { kind, operation } = getMainDefinition(query);
+    return kind === 'OperationDefinition' && operation === 'subscription';
+  },
+  wsLink,
+  httpLink,
+);
 
 const client = new ApolloClient({
-  link: ApolloLink.from([
-    onError(({ graphQLErrors, networkError }) => {
-      if (graphQLErrors)
-        graphQLErrors.map(({ message, locations, path }) =>
-          console.log(
-            `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`,
-          ),
-        );
-      if (networkError) console.log(`[Network error]: ${networkError}`);
-    }),
-    new HttpLink({
-      uri: 'http://localhost:8080/graphql',
-      credentials: 'same-origin'
-    })
-  ]),
+  link,
   cache: new InMemoryCache()
 });
 
 // Pass your GraphQL endpoint to uri
-// const client = new ApolloClient({ uri: 'http://localhost:8080/graphql' });
 
 const ApolloApp = AppComponent => (
   <ApolloProvider client={client}>
